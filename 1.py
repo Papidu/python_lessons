13521c2110bb19f8251c78d112854612974e39e9
Vladimir Podprajtov <vladimirpodpratov@gmail.com>
2023-03-25 16:18:03 +0500

    add action motion

--- 1.py
+++ 1.py
@@ -6,8 +6,11 @@ window = tk.Tk()
 def change():
     button['background'] = 'green'
 
+def change1(e):
+    button['background'] = 'green'
 
 button = tk.Button(text='GUESS', width=10, height=3, command=change)
+button.bind('<Motion>', change1)
 button.pack()
 
 window.mainloop()
